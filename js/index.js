3f4d8717d555d976361241b5aaef3628c9649696
Novak Fenocchio <fenomartin6@gmail.com>
2021-03-09 11:49:24 -0300

    nav menu horizontal

---
+++ js/index.js
@@ -0,0 +1,16 @@
+let hiddenMenu = false;
+
+$('.lateral-menu').hide();
+
+$('.menu-mobile').on('click', function(){
+    hiddenMenu = !hiddenMenu;
+
+    if(hiddenMenu)
+    {
+        $('.lateral-menu').show();
+    }else
+    {
+        $('.lateral-menu').hide();
+    }
+})
+
